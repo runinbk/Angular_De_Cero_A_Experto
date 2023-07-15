a9c63c1883e9bdb8aa896e6a2db45a8c35579df2
runinbk <runinbk@gmail.com>
2023-07-15 10:32:00 -0400

    39. Funcionalidad del Contador

--- 2023/projects/02-bases/src/app/counter/counter.component.ts
+++ 2023/projects/02-bases/src/app/counter/counter.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-counter',
-    template: '<h1>Hola Mundo desde el Counter</h1>'
+    template: `
+        <h3>Counter : {{ counter }}</h3>
+
+        <button (click)="increaseBy(+1)">+1</button>
+        <button (click)="reset()">Reset</button>
+        <button (click)="increaseBy(-1)">-1</button>
+
+    `
 })
 
 export class CounterComponent {
+    public counter: number = 10;
 
+    increaseBy(value: number): void {
+        this.counter += value;
+    }
+
+    reset(): void {
+        this.counter = 10;
+    }
 }
