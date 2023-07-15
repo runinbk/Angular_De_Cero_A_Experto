b75374256f44becbc591592bedfb4eb77504d419
runinbk <runinbk@gmail.com>
2023-07-15 17:25:09 -0400

    5.5. @Input - Reciviendo del padre

--- 2023/projects/02-bases/src/app/dbz/pages/main-page.component.ts
+++ 2023/projects/02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
     selector: 'app-dbz-main-page',
     templateUrl: 'main-page.component.html'
 })
 
-export class MainPegeComponent { }
+export class MainPegeComponent {
+
+    public characters: Character[] = [
+        {
+            name: 'Krillin',
+            power: 1000
+        },
+        {
+            name: "Goku",
+            power: 9500,
+        },
+        {
+            name: 'Vegeta',
+            power: 7500
+        }
+    ];
+
+}
