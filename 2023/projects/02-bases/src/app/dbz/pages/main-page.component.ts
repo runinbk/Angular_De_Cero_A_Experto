b69fa71944745e73b6081b38121002e9b89527da
runinbk <runinbk@gmail.com>
2023-07-16 13:34:54 -0400

    5.13. Servicio privado

--- 2023/projects/02-bases/src/app/dbz/pages/main-page.component.ts
+++ 2023/projects/02-bases/src/app/dbz/pages/main-page.component.ts
@@ -9,6 +9,18 @@ import { DbzService } from '../services/dbz.service';
 
 export class MainPegeComponent {
 
-    constructor(public dbzService: DbzService) { }
+    constructor(private dbzService: DbzService) { }
+
+    get characters(): Character[] {
+        return [...this.dbzService.characters];
+    }
+
+    onDeleteCharacter(id: string): void {
+        this.dbzService.onDeleteCharacter(id);
+    }
+
+    onNewCharacter(character: Character): void {
+        this.dbzService.onNewCharacter(character);
+    }
 
 }
