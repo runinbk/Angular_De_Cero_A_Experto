c93b4f5cdca44e106e59d597a989414a6e4e1384
runinbk <runinbk@gmail.com>
2023-07-17 15:04:45 -0400

    9.9. Mostrar los paises en pantalla

--- 2023/projects/04-contryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 2023/projects/04-contryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,8 +10,15 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
-  searchByCapital(trem: string): void {
-    console.log('Search by capital:', trem);
+  public countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) { }
+
+  searchByCapital(term: string): void {
+    this.countriesService.searchCapital(term)
+      .subscribe(countries => {
+        this.countries = countries;
+      });
   }
 
 }
