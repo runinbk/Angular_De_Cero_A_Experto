140ded2f98cce43a49eb5967086d2c30d5e1fa71
runinbk <runinbk@gmail.com>
2023-07-17 10:04:16 -0400

    8.5. Animaciones de CSS

--- 2023/projects/03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 2023/projects/03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -20,8 +20,9 @@ export class LazyImageComponent implements OnInit {
   }
 
   onLoad() {
-    console.log('Image loaded')
-    this.hasLoaded = true;
+    setTimeout(() => {
+      this.hasLoaded = true;
+    }, 1000)
   }
 
 }
