86c168da8c5458f4aa4ee9edf4118264e3e56dd1
runinbk <runinbk@gmail.com>
2023-07-17 09:37:25 -0400

    8.3. Lazy Image - Parte 1

--- 2023/projects/03-gifs-app/src/app/gifs/gifs.module.ts
+++ 2023/projects/03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
 import { HomeComponent } from './pages/home/home.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
@@ -15,7 +17,8 @@ import { GifCardComponent } from './components/gif-card/gif-card.component';
     GifCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomeComponent,
