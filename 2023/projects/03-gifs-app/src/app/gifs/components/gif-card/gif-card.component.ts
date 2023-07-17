7718deb449b08035f54ccf7be75301629ad0f700
runinbk <runinbk@gmail.com>
2023-07-17 08:29:14 -0400

    8.2. Resolucion de la tarea

--- 2023/projects/03-gifs-app/src/app/gifs/components/gif-card/gif-card.component.ts
+++ 2023/projects/03-gifs-app/src/app/gifs/components/gif-card/gif-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Gif } from '../../interfaces/gif.interfaces';
 
 @Component({
   selector: 'app-gif-card',
   templateUrl: './gif-card.component.html',
   styleUrls: ['./gif-card.component.css']
 })
-export class GifCardComponent {
+export class GifCardComponent implements OnInit {
+
+  @Input()
+  public gif!: Gif;
+
+  ngOnInit(): void {
+    if (!this.gif) throw new Error('Gif property is required')
+  }
 
 }
