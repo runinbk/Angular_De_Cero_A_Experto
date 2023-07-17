8c59374b6ca617b96d80423830a534a26197bfad
runinbk <runinbk@gmail.com>
2023-07-17 12:50:34 -0400

    9.8. Countries Services - Restcountries

--- 2023/projects/04-contryApp/src/app/app.module.ts
+++ 2023/projects/04-contryApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SharedModule } from './shared/shared.module';
 
@@ -13,6 +14,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+
     SharedModule
   ],
   providers: [],
